d5ad3455229a8ac7f8e49b4f131deb4ecfbd6b97
BrianR <briomero76@gmail.com>
2021-01-06 22:04:04 -0500

    little changes in the classes

--- src/model/Type.java
+++ src/model/Type.java
@@ -1,12 +1,25 @@
 package model;
-
+import java.util.Random;
 public class Type{
   private final String[][] classes = {{"","",""},
                                       {"","",""},
                                       {"","",""}};
   private String type;
-  
+  private Faction faction;
+  private Random choose;
+  private  String Class;
   public Type(){
     type = new String();
+    choose = new Random();
+    faction = setFaction();
+    Class = new String();
   }//End constructor
+  public void setType(int level){
+    Class = classes[faction.ordinal()][];
+  }//End getType
+  private Faction setFaction(){
+    Faction[] fact = Faction.values();
+    Faction faction = fact[(choose.nextInt(3))];
+    return faction;
+  }//End setFaction
 }//End class Type
